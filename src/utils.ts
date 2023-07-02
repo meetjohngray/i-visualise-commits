55135959ffdf3fd53178a38737c740b9aed1929a
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-03 07:23:59 +1200

    prettier

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,18 @@
 export function standardDeviation(values: number[]) {
-	const avg = average(values);
-	const squareDiffs = values.map(function (value) {
-		const diff = value - avg;
-		const sqrDiff = diff * diff;
-		return sqrDiff;
-	});
+	const avg = average(values)
+	const squareDiffs = values.map(function(value) {
+		const diff = value - avg
+		const sqrDiff = diff * diff
+		return sqrDiff
+	})
 
-	const avgSquareDiff = average(squareDiffs);
-	const stdDev = Math.sqrt(avgSquareDiff);
-	return stdDev;
+	const avgSquareDiff = average(squareDiffs)
+	const stdDev = Math.sqrt(avgSquareDiff)
+	return stdDev
 }
 
 function average(data: number[]) {
-	const sum = data.reduce((sum, value) => sum + value, 0);
-	const avg = sum / data.length;
-	return avg;
+	const sum = data.reduce((sum, value) => sum + value, 0)
+	const avg = sum / data.length
+	return avg
 }
