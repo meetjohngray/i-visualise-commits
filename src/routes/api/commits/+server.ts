1a2a41298836b5cdb009976d1e7ac2d4a4fc9c59
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-02 17:42:12 +1200

    add login to post route

--- src/routes/api/commits/+server.ts
+++ src/routes/api/commits/+server.ts
@@ -1,7 +1,115 @@
+import { PrismaClient } from '@prisma/client'
+import * as z from 'zod'
 import type { RequestEvent } from './$types'
 
+const schema = z.object({
+  ref: z.string(),
+  repository: z.object({
+    name: z.string(),
+    created_at: z.number()
+  }),
+  organization: z.object({
+    login: z.string()
+  }),
+  sender: z.object({
+    site_admin: z.boolean()
+  }),
+  commits: z.array(
+    z.object({
+      id: z.string(),
+      timestamp: z.string(),
+      committer: z.object({
+        name: z.string(),
+        email: z.string(),
+        username: z.string()
+      })
+    })
+  )
+})
 export async function POST({ request }: RequestEvent) {
-  const payload = await request.json()
-  console.log(payload)
-  return new Response(null, { status: 201 })
+  console.log('requested')
+
+  const prisma = new PrismaClient()
+  try {
+    const body = await request.json()
+
+    // validate the request body
+    const result = schema.safeParse(body)
+
+    if (!result.success) {
+      return new Response(JSON.stringify(result.error), {
+        status: 400,
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+    }
+
+    const payload = result.data
+
+    for (const commit of payload.commits) {
+      await prisma.student.upsert({
+        where: {
+          email: commit.committer.email
+        },
+        update: {
+          name: commit.committer.name,
+          email: commit.committer.email,
+          is_student: true
+        },
+        create: {
+          name: commit.committer.name,
+          email: commit.committer.email,
+          is_student: true
+        }
+      })
+    }
+
+    await prisma.repo.upsert({
+      where: {
+        name: payload.repository.name
+      },
+      update: {
+        forked_date: new Date(payload.repository.created_at),
+        name: payload.repository.name
+      },
+      create: {
+        forked_date: new Date(payload.repository.created_at),
+        name: payload.repository.name
+      }
+    })
+
+    for (const commit of payload.commits) {
+      await prisma.commit.upsert({
+        where: {
+          id: commit.id
+        },
+        update: {
+          created_on: commit.timestamp,
+          email: commit.committer.email,
+          repo_name: payload.repository.name,
+          branch: payload.ref,
+          id: commit.id
+        },
+        create: {
+          created_on: commit.timestamp,
+          email: commit.committer.email,
+          repo_name: payload.repository.name,
+          branch: payload.ref,
+          id: commit.id
+        }
+      })
+    }
+
+    return new Response('OK', { status: 201 })
+  } catch (error) {
+    console.log(error)
+    await prisma.log.create({
+      data: {
+        error: JSON.stringify(error)
+      }
+    })
+
+    return new Response(JSON.stringify(error), { status: 500 })
+  }
 }
