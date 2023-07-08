565c6e2df06e35a486086e01f61b8fd439f4267e
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-07-08 15:19:57 +1200

    install faker and add fake data

--- prisma/db.ts
+++ prisma/db.ts
@@ -11,40 +11,62 @@ type Commit = Prisma.CommitGetPayload<{
   }
 }>
 
-function generateFakeRepos() {
-  // will use the company name to generate 20 fake repo name
-  faker.seed(123)
-  const repos = Array.from({ length: 3 }, () => faker.company.name())
-  return repos
-}
-
 function getFakeData(): Commit[] {
-  let commits: Commit[] = []
+  // Challenges names
+  const challenges = [
+    'Challenge 1',
+    'Challenge 2',
+    'Challenge 3',
+    'Challenge 4',
+    'Challenge 5',
+    'Challenge 6',
+    'Challenge 7',
+    'Challenge 8',
+    'Challenge 9',
+    'Challenge 10',
+    'Challenge 11',
+    'Challenge 12',
+    'Challenge 13',
+    'Challenge 14',
+    'Challenge 15'
+  ]
 
-  const fakeRepos = generateFakeRepos()
-  faker.seed(123)
+  const students = []
+
+  // generate 10 students
   for (let i = 0; i < 10; i++) {
-    // create a random number
-    const randomGitHubId = Math.floor(Math.random() * 100)
-
-    const student = {
+    students.push({
       name: faker.person.firstName(),
       username: faker.internet.userName(),
-      github_id: randomGitHubId
-    }
-
-    const randomBetween1And10 = Math.floor(Math.random() * 10) + 1
-    const studentCommits = Array.from({ length: randomBetween1And10 }, () => ({
-      username: faker.person.firstName(),
-      branch: faker.git.branch(),
-      created_on: faker.date.past(),
-      repo_name: faker.helpers.arrayElement(fakeRepos),
-      Student: student
-    }))
-
-    commits.push(...studentCommits)
-    faker.seed(randomGitHubId)
+      github_id: Math.floor(Math.random() * 1000000)
+    })
   }
+
+  const commits: Commit[] = []
+
+  // generate commits for each student
+  students.forEach((student) => {
+    // a student makes 20 to 100 commits
+    const commitCount = Math.floor(Math.random() * 80) + 20
+
+    for (let j = 0; j < commitCount; j++) {
+      // randomly select a challenge
+      const challenge =
+        challenges[Math.floor(Math.random() * challenges.length)]
+
+      commits.push({
+        username: student.username,
+        branch: 'main',
+        created_on: faker.date.past(),
+        repo_name: challenge,
+        Student: {
+          name: student.name,
+          username: student.username,
+          github_id: student.github_id
+        }
+      })
+    }
+  })
 
   return commits
 }
